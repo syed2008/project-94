42987a71cec898967a0cfe608c08620ed3a89471
syed2008 <78297824+syed2008@users.noreply.github.com>
2021-06-15 22:42:44 +0530

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBBQi4BE_UULFOU4sjcvp2Av4mUy1Shqic",
+    authDomain: "kwitter-bfcc3.firebaseapp.com",
+    databaseURL: "https://kwitter-bfcc3-default-rtdb.firebaseio.com",
+    projectId: "kwitter-bfcc3",
+    storageBucket: "kwitter-bfcc3.appspot.com",
+    messagingSenderId: "1025457391078",
+    appId: "1:1025457391078:web:f1b669e0137d7c6bc70227"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
